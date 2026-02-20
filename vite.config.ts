6f64c962e62a7ebd46caf57c8bcd578db2a8e1d5
OpenAudioOER <openaudio@fastmail.com>
2026-02-20 12:08:37 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // This ensures your assets load from /listen/ instead of the root domain
+      base: '/listen/', 
       server: {
         port: 3000,
         host: '0.0.0.0',
